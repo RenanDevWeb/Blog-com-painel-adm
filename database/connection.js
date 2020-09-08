3f4f500adb5dde358bb38fb796b3ec0da11acc80
Renan <renansalmazio@gmail.com>
2020-09-08 00:09:03 -0300

    Adicionando dotenv ao projeto

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,12 @@
 const Sequelize = require("sequelize")
+const dotenv = require('dotenv');
+dotenv.config()
 
-const databaseName = 'guia_press'
-const user = 'root'
-const password = 'root'
-const host = 'localhost'
-const dialect = 'mysql'
+const databaseName = process.env.DB_NAME
+const user = process.env.BD_USER || 'xxxxxxx'
+const password = process.env.DB_PASS || 'xxxxxx'
+const host =  process.env.DB_HOST
+const dialect = process.env.BD_DIALECT
 
 const connection = new Sequelize(databaseName,user,password, {
     host,
