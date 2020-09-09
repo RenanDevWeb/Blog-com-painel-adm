c412f1617565c018b3bdd2227e61ce32dba36ee0
Renan <renansalmazio@gmail.com>
2020-09-08 22:43:21 -0300

    adicionado artigos ao blog

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,14 +1,97 @@
 const express = require('express')
 const router = express.Router()
+const Category = require("../categories/Category")
+const Article = require("../articles/Article")
+const slugify = require('slugify')
 
 
 
-router.get('/articles',(req,res) => {
-    res.send('Rota Articles')
+router.get('/admin/articles',(req,res) => {
+        Article.findAll({
+            include: [{model: Category}]
+        }).then(articles => {
+
+            res.render('admin/articles/index', {
+                articles: articles
+            })
+        })
+
 })
 router.get('/admin/articles/new',(req,res) => {
-    res.send('Rota Categories')
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories})
+    })
 })
 
+router.get('/admin/articles/edit/:id', (req,res) => {
+    const id = req.params.id        
+    
+    Article.findByPk(id).then(articles => {
+        if(articles != undefined){
+            Category.findAll().then(categories => {
+                res.render('admin/articles/edit',{
+                    articles: articles,
+                    categories: categories
+                })
+            })
+                
+        }else{
+            res.render('admin/articles')
+        }
+    }).catch(err => res.redirect('/admin/articles'))
+    })
+
+
+
+router.post('/admin/articles/save', (req,res) => {
+    const title = req.body.title
+    const body = req.body.body
+    const category = req.body.category
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category 
+    }).then(() => {
+        res.redirect('/admin/articles')
+    })
+})
+
+router.post('/articles/delete',(req,res) => {
+    const id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Article.destroy({
+                where:{
+                    id:id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles")
+            }).catch(err => console.log(err))
+        }else{
+            res.redirect("/admin/articles")
+        }
+    }else{
+        res.redirect("/admin/articles")
+    }
+})
+
+router.post('/articles/update',(req,res)=> {
+    const id = req.body.id
+    const title = req.body.title
+    const body = req.body.body
+    const category = req.body.category
+    Article.update({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    },{
+        where: {
+            id: id
+        }
+    }).then(() =>  { res.redirect("/admin/articles")
+}).catch(err => res.redirect("/"))
+})
 
 module.exports = router;
--- database/connection.js
+++ database/connection.js
@@ -3,14 +3,16 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 const databaseName = process.env.DB_NAME
-const user = process.env.BD_USER || 'xxxxxxx'
-const password = process.env.DB_PASS || 'xxxxxx'
+const user = process.env.BD_USER || 'root'
+const password = process.env.DB_PASS || 'root'
 const host =  process.env.DB_HOST
 const dialect = process.env.BD_DIALECT
+const timezone = "-03:00"
 
 const connection = new Sequelize(databaseName,user,password, {
     host,
-    dialect
+    dialect,
+    timezone, 
 })
 
 
